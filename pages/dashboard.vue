4c63fe8518342a0e4e07ccfa0dda3f87db0f7b70
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-11 00:30:47 +0200

    Add first dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -2,8 +2,40 @@
 definePageMeta({
   middleware: ["auth"],
 });
+
+import {Bar} from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const chartData = {
+  labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 39, 10, 40, 39, 80, 40]
+    }
+  ]
+}
+
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+
 </script>
 
 <template>
-  <p>Dashboard</p>
+  <div>
+    <p>Dashboard</p>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <Bar
+          id="my-chart-id"
+          :options="chartOptions"
+          :data="chartData"
+      />
+    </div>
+  </div>
 </template>
