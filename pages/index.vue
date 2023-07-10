948df14a4061f2ceed9c4c3d934aa9b391f72e2e
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-10 23:31:35 +0200

    Init daisy ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <p>Test</p>
+  <button class="btn">Button</button>
+  <button class="btn btn-neutral">Neutral</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-secondary">Button</button>
+  <button class="btn btn-accent">Button</button>
+  <button class="btn btn-ghost">Button</button>
+  <button class="btn btn-link">Button</button>
 </template>
---
+++ server/middleware/check.ts
@@ -0,0 +1,17 @@
+import {serverSupabaseUser} from '#supabase/server';
+export default defineEventHandler(async (event) => {
+    const path = getRequestPath(event);
+    const splitPath = path.split('/');
+
+    if (splitPath[1] === 'api' && splitPath[2] !== 'middleware' && splitPath[2] !== '_supabase') {
+        const currentUser =await serverSupabaseUser(event);
+
+        const userData = await $fetch('/api/middleware/get?id=' + currentUser.id, {
+            method: 'get',
+        });
+
+        if (userData) {
+            event.context.auth = { user: userData[0] }
+        }
+    }
+})
