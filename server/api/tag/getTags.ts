962342351366b6df13702731f8f9714f68ea37cb
waseem1104 <waseem11@hotmail.fr>
2023-07-12 01:17:57 +0200

    Get tags by current user

--- server/api/tag/getTags.ts
+++ server/api/tag/getTags.ts
@@ -4,9 +4,12 @@ export default defineEventHandler(async (event) => {
 
     const supabase = serverSupabaseServiceRole(event);
 
+    const { data: { user } } = await supabase.auth.getUser(event.context._token)
+
     const {data, error} = await supabase
         .from('tag')
         .select('*')
+        .eq('user_id', user!.id);
 
     if(error) {
         return 'Error';
