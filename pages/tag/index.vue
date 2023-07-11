2aea2a586f7a5af6614acf0e2a4b77f4153ac4ec
waseem1104 <waseem11@hotmail.fr>
2023-07-12 00:08:16 +0200

    Init page tag

---
+++ pages/tag/index.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div class="p-10">
+        <div class="flex justify-end">
+            <button class="btn">Créer un tag</button>
+        </div>
+
+        <div class="mt-10 overflow-x-auto">
+            <table class="table">
+                <!-- head -->
+                <thead>
+                <tr>
+                    <th></th>
+                    <th>Commentaire</th>
+                 
+                </tr>
+                </thead>
+                <tbody>
+                <!-- row 1 -->
+                <tr class="bg-base-200">
+                    <th>1</th>
+                    <td>Cy Ganderton</td>
+                    
+                </tr>
+                <!-- row 2 -->
+                <tr>
+                    <th>2</th>
+                    <td>Hart Hagerty</td>
+                   
+                </tr>
+                <!-- row 3 -->
+                <tr>
+                    <th>3</th>
+                    <td>Brice Swyre</td>
+                   
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+// definePageMeta({
+//   middleware: ["auth"],
+// });
+
+</script>
