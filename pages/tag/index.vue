650730cfd6097bda90507482e7779ecd99bae77c
waseem1104 <waseem11@hotmail.fr>
2023-07-12 01:12:59 +0200

    Tag created with modal

--- pages/tag/index.vue
+++ pages/tag/index.vue
@@ -2,8 +2,22 @@
 
     <div class="p-10">
         <div class="flex justify-end">
-            <button class="btn">Créer un tag</button>
+            <button class="btn" onclick="modal_tag.showModal()">Créer un tag</button>
         </div>
+
+        <dialog id="modal_tag" class="modal">
+            <form method="dialog" class="modal-box" @submit.prevent="handleSubmit">
+                <h3 class="font-bold text-lg">Créer un tag</h3>
+
+                <div class="mt-5">
+                    <input v-model="comment" type="text" placeholder="Commentaire..." class="input input-bordered w-full" />
+                    <button type="submit" class="btn mt-5">Enregistrer</button>
+                </div>
+            </form>
+            <form method="dialog" class="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
 
         <div class="mt-10 overflow-x-auto">
             <table class="table">
@@ -35,7 +49,9 @@
 definePageMeta({
   middleware: ["auth"],
 });
+
 const tags = ref({});
+const comment = ref("");
 
 async function getTags(){
 
@@ -48,10 +64,23 @@ async function getTags(){
   }
 }
 
-onMounted( async () => {
+const handleSubmit = async () => {
+   
+   const data = await $fetch('/api/tag/new', {
+        method: 'post',
+         body: {
+            comment: comment.value,
+        }
+    });
+
+    comment.value = "";
 
     getTags();
 
+}
+
+onMounted( async () => {
+    getTags();
 })
 
 </script>
