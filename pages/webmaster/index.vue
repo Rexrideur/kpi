dccdda0534ae95c7b2e2c3f62dea236b14e35bef
jehanebnj <jehanebnj@gmail.com>
2023-07-13 00:41:59 +0200

    Implement list all webmaster validated

--- pages/webmaster/index.vue
+++ pages/webmaster/index.vue
@@ -5,17 +5,44 @@
                 <!-- head -->
                 <thead>
                 <tr>
-                    <th></th>
                     <th>Nom</th>
                     <th>Prénom</th>
                     <th>Email</th>
                     <th>Nom de la société</th>
-                    <th>RIB</th>
+                    <th>KBIS</th>
                 </tr>
                 </thead>
                 <tbody>
+                    <tr v-for="webmaster in webmasters" :key="webmaster.id">
+                        <th>{{webmaster.lastname}}</th>
+                        <td>{{webmaster.firstname}}</td>
+                        <td>{{webmaster.email}}</td>
+                        <td>{{webmaster.company_name}}</td>
+                        <td>{{webmaster.kbis}}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
     </div>
 </template>
+
+<script setup>
+
+const webmasters = ref({});
+
+async function getWebmasterValidated(){
+
+    const data = await $fetch('/api/webmaster/getAll', {
+        method: 'get',
+    });
+
+    if (data !== 'Error') {
+        webmasters.value = data; 
+    }
+}
+
+onMounted( () => {
+
+    getWebmasterValidated();
+})
+</script>
