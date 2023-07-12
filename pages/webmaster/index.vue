9eb18008c1fe284a13ef990536806ebcc5f53110
jehanebnj <jehanebnj@gmail.com>
2023-07-13 00:31:45 +0200

    Create page for list webmaster - admin

---
+++ pages/webmaster/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="p-10">
+        <div class="mt-10 overflow-x-auto">
+            <table class="table">
+                <!-- head -->
+                <thead>
+                <tr>
+                    <th></th>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Email</th>
+                    <th>Nom de la société</th>
+                    <th>RIB</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
