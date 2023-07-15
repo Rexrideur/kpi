04c65f4676fa4d61a5029cbcbd152a9ee427cbea
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-15 19:40:08 +0200

    Add dynamic filter chart

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -4,75 +4,130 @@ definePageMeta({
   layout: "custom",
 });
 
-import {Bar} from 'vue-chartjs'
-import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-const chartData = {
-  labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
-  datasets: [
-    {
-      label: 'Data One',
-      backgroundColor: '#f87979',
-      data: [40, 39, 10, 40, 39, 80, 40]
-    }
-  ]
-}
-
-const chartOptions = {
-  responsive: true,
-  maintainAspectRatio: false
-}
+import Chart from 'chart.js/auto';
 
 const chartDataClick = {
   labels: [],
   datasets: [
     {
-      label: 'Data One',
+      label: 'Nombre de clics par bouton',
       backgroundColor: '#f87979',
       data: []
     }
   ]
 }
 
-const chartOptionsClick = {
-  responsive: true,
-  maintainAspectRatio: false
-}
+const configClick = {
+  type: 'bar',
+  data: chartDataClick,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+};
+
+let chartClick = null;
 
 
 const supabase = useSupabaseClient();
-const showClickChart = ref(false);
+const filterClick = ref('days');
 
-onMounted(() => {
-  console.log('Component is mounted');
-  const {data} = supabase
-      .from('button_analytics')
-      .select('*')
-      .then(({data, error}) => {
-        if (error) {
-          console.log(error)
-        } else {
-          const buttonCounts = {};
-          let button_names = [];
-          let button_data = [];
+onMounted(async () => {
+  chartClick = new Chart(document.getElementById('chart-click'), configClick);
+  getClicks();
+});
 
-          data.forEach((item) => {
-            if (buttonCounts[item.button_name]) {
-              buttonCounts[item.button_name]++;
-            } else {
-              buttonCounts[item.button_name] = 1;
-            }
-          });
-          button_names = Object.keys(buttonCounts);
-          button_data = Object.values(buttonCounts);
-          chartDataClick.labels = button_names;
-          chartDataClick.datasets[0].data = button_data;
-          showClickChart.value = true;
-        }
-      })
-})
+watch(filterClick, () => {
+  getClicks();
+});
+
+async function getClicks () {
+  let filter = null;
+  switch (filterClick.value) {
+    case 'days':
+    {
+      let currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() - 7);
+
+      const year = currentDate.getFullYear();
+      const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+      const day = ("0" + currentDate.getDate()).slice(-2);
+
+      filter = year + "-" + month + "-" + day;
+    }
+      break;
+    case 'month':
+    {
+      let currentDate = new Date();
+      currentDate.setMonth(currentDate.getMonth() - 1);
+
+      const year = currentDate.getFullYear();
+      const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+      const day = ("0" + currentDate.getDate()).slice(-2);
+
+      filter = year + "-" + month + "-" + day;
+    }
+      break;
+  }
+
+  if (filterClick.value !== 'all') {
+    await supabase
+        .from('button_analytics')
+        .select('*')
+        .gte('date', filter)
+        .then(({data, error}) => {
+          if (error) {
+            console.log(error)
+          } else {
+            const buttonCounts = {};
+            let button_names = [];
+            let button_data = [];
+
+            data.forEach((item) => {
+              if (buttonCounts[item.button_name]) {
+                buttonCounts[item.button_name]++;
+              } else {
+                buttonCounts[item.button_name] = 1;
+              }
+            });
+            button_names = Object.keys(buttonCounts);
+            button_data = Object.values(buttonCounts);
+            chartDataClick.labels = button_names;
+            chartDataClick.datasets[0].data = button_data;
+            chartClick.update();
+          }
+        })
+  }else{
+    await supabase
+        .from('button_analytics')
+        .select('*')
+        .then(({data, error}) => {
+          if (error) {
+            console.log(error)
+          } else {
+            const buttonCounts = {};
+            let button_names = [];
+            let button_data = [];
+
+            data.forEach((item) => {
+              if (buttonCounts[item.button_name]) {
+                buttonCounts[item.button_name]++;
+              } else {
+                buttonCounts[item.button_name] = 1;
+              }
+            });
+            button_names = Object.keys(buttonCounts);
+            button_data = Object.values(buttonCounts);
+            chartDataClick.labels = button_names;
+            chartDataClick.datasets[0].data = button_data;
+            chartClick.update();
+          }
+        })
+  }
+}
 
 </script>
 
@@ -80,18 +135,18 @@ onMounted(() => {
   <div>
     <div class="grid grid-cols-2 gap-4">
       <div class="card w-full bg-base-100 shadow-xl p-10">
-        <Bar
-            id="my-chart-id"
-            :options="chartOptions"
-            :data="chartData"
-        />
-      </div>
-      <div class="card w-full bg-base-100 shadow-xl p-10" v-if="showClickChart">
-        <Bar
-            id="chart-click"
-            :options="chartOptionsClick"
-            :data="chartDataClick"
-        />
+        <div>
+          <select class="select select-bordered w-full max-w-xs" v-model="filterClick">
+            <option value="days">7 jours</option>
+            <option value="month">1 mois</option>
+            <option value="all">Depuis le début</option>
+          </select>
+        </div>
+        <div class="h-full mt-5">
+          <canvas
+              id="chart-click"
+          ></canvas>
+        </div>
       </div>
     </div>
   </div>
